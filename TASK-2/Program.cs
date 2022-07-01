e8c42de44b69fb1dd3a805d615a0ff483cfc1a34
Vlad <vlad.melnik@bk.ru>
2022-07-01 22:53:17 +0300

    TASK-1 added

---
+++ TASK-2/Program.cs
@@ -0,0 +1,17 @@
+﻿string Third(int num)
+{
+    int result = 1;
+    if(num < 100) return ("There is no third digit in your number");
+    else
+    {
+        while ((num / 100) >= 10)
+        {
+        num /= 10;
+        }
+        result = num % 10;
+        return ("Third digit: " + result);
+    }
+}
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Third(num));
