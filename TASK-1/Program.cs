3fe1b37e93e463a50c3cd9838618cc1d2c2d0eb1
Vlad <vlad.melnik@bk.ru>
2022-07-02 12:06:13 +0300

    1 Task added

---
+++ TASK-1/Program.cs
@@ -0,0 +1,11 @@
+﻿int Second()
+{
+  int num = new Random().Next(100, 1000);
+  Console.WriteLine("Current number is " + num);
+  int des = (num%100)/10;
+  int result = des;
+  return result;
+}
+
+int number = Second();
+Console.WriteLine("Result number is " + number);
