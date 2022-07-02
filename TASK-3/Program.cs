aca08b41ad296b7f4b7734639552951c1a796ddd
Vlad <vlad.melnik@bk.ru>
2022-07-02 12:08:46 +0300

    Third task added

--- TASK-3/Program.cs
+++ TASK-3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void dayweek(int tatum)
+{
+    if(tatum == 6) 
+        Console.WriteLine("TRUE");
+    if(tatum == 7)
+        Console.WriteLine("TRUE");
+    else
+    if(tatum>8)
+        Console.WriteLine("FALSE");
+    if(tatum>e8c42de44b69fb1dd3a805d615a0ff483cfc1a348)
+        Console.WriteLine("FALSE");  
+}
+Console.WriteLine("Input a number:");
+int task = Convert.ToInt32(Console.ReadLine());
+dayweek(task);
